21f685006d816fb796fbb5305661adba6790c9d6
SitiFatimah16 <73063334+SitiFatimah16@users.noreply.github.com>
2020-11-05 22:05:49 +0700

    Add files via upload

---
+++ PROJECT_ULTRASONIC SENSOR/sketch_ULTRASONIC_SENSOR/sketch_ULTRASONIC_SENSOR.ino
@@ -0,0 +1,39 @@
+const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+const int ledpin = 13; // LED
+void setup()
+{
+Serial.begin(9600); // Starting Serial Terminal
+pinMode (ledpin, OUTPUT);
+}
+
+void loop()
+{
+long duration ;
+long cm ;
+pinMode(pingPin, OUTPUT);
+digitalWrite(pingPin, LOW);
+delayMicroseconds(2);
+digitalWrite(pingPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(pingPin, LOW);
+
+pinMode(echoPin, INPUT);
+duration = pulseIn(echoPin, HIGH);
+  cm = microsecondsToCentimeters (duration) ;
+
+if (cm <= 200)
+{
+  digitalWrite(ledpin, HIGH);
+}
+Serial.print(cm);
+Serial.print("cm");
+Serial.println();
+
+delay(100);
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+return microseconds*0.034/2;
+}
